c24cec0311878165de249198c70fa348a0480b9d
danielemanuel <daniel.costea@vkernel.ro>
2017-07-04 19:18:10 +0100

    Connect MongoDB to the nodejs server using mongoose

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,14 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/messages/:msg', function(req, res, next) {
+  res.render('messages', {message: req.params.msg});
+});
+
+router.post('/messages', function(req, res, next) {
+  var message = req.body.message;
+  res.redirect('/messages/' + message)
+});
+
+
 module.exports = router;

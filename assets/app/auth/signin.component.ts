4ae96a70966add9801d9d2056e38dc1357acfabe
danielemanuel <daniel.costea@vkernel.ro>
2017-07-11 11:26:57 +0100

    * user can sign in * BE wired up with FE * JWT implemented for password configuration

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from "@angular/core"
+import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormControl, Validators } from "@angular/forms";
+
+import { AuthService } from './auth.service';
+import { User } from './user.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -9,8 +13,22 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 export class SigninComponent {
   myForm: FormGroup;
 
-  onSubmit() {
+  constructor(private authService: AuthService, private router: Router) {}
 
+  onSubmit() {
+    const user = new User(
+      this.myForm.value.email,
+      this.myForm.value.password
+    );
+    this.authService.signin(user)
+      .subscribe(
+        data => {
+          localStorage.setItem('token', data.token);
+          localStorage.setItem('userId', data.userId);
+          this.router.navigateByUrl('/');
+        },
+      error => console.error(error)
+    );
     this.myForm.reset();
   }
 
@@ -18,7 +36,7 @@ export class SigninComponent {
     this.myForm = new FormGroup({
       email: new FormControl(null, [
         Validators.required,
-        Validators.pattern('[a-zA-Z ]*')
+        Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")
       ]),
       password: new FormControl(null, Validators.required)
 
